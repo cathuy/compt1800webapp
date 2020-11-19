3860483f64e62f661b27fd0058a88da118a5a9c7
Dinh Phi Long Nguyen <superitboss@gmail.com>
2020-11-19 14:39:48 -0800

    Finish list of restarant database attach with the list of restarant

--- scripts/listRestaurant.js
+++ scripts/listRestaurant.js
@@ -1,13 +1,32 @@
 
-$(document).ready(function res1(){
+$(document).ready(function res(){
     db.collection("restaurant").doc("01")
         .onSnapshot(function(q){
             //document.getElementById("res1").innerText = q.data().name;
             $("#resName1").append(q.data().name);
             $("#resType1").append(q.data().typeOfCuisine);
-
             var address = q.data().Address;
             $("#resAdd1").append(address.Streetnumber + " " + address.City);
-        
+
+    })
+
+    db.collection("restaurant").doc("02")
+        .onSnapshot(function(q){
+            //document.getElementById("res1").innerText = q.data().name;
+            $("#resName2").append(q.data().name);
+            $("#resType2").append(q.data().typeOfCuisine);
+            var address = q.data().Address;
+            $("#resAdd2").append(address.streetNumber + " " + address.city);
+
+    })
+
+    db.collection("restaurant").doc("03")
+        .onSnapshot(function(q){
+            //document.getElementById("res1").innerText = q.data().name;
+            $("#resName3").append(q.data().name);
+            $("#resType3").append(q.data().typeOfCuisine);
+            var address = q.data().Address;
+            $("#resAdd3").append(address.streetNumber + " " + address.city);
+
     })
 })
