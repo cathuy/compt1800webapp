9c0fe7278e59664e2c551ab17a0fb8344f19adcb
Thuy Le <ca.happy95@gmail.com>
2020-11-27 13:16:27 -0800

    nothing

--- scripts/listRestaurant.js
+++ scripts/listRestaurant.js
@@ -4,13 +4,12 @@ function displaypromotions(promotionid, restaurantid){
         const pickup = q.data().pickup;
         const reservation = q.data().reservation;
         var small = "<small></small>"
-        console.log(delivery, pickup, reservation);
         if (delivery > 0) {
-            small= "<small>" + q.data().delivery + "% for delivery order";
+            small= "<small>" + q.data().delivery + "% off for delivery order";
         } else if (pickup != 0) {
-            small = "<small>" + q.data().pickup + "% for pickup order</small>";
+            small = "<small>" + q.data().pickup + "% off for pickup order</small>";
         } else if (reservation != 0) {
-            small="<small>" + q.data().reservation + "</small>";
+            small="<small>" + q.data().reservation + "% off for reservation</small>";
         }
         $("#"+restaurantid).append(small);
     });
@@ -19,7 +18,6 @@ function listRestaurant() {
     db.collection("restaurant").get().then(function(q){
         var id, address, image, name, cuisine, small;
         q.forEach(function(doc){
-            console.log(doc.id, "=>", doc.data())
             id = doc.data().id;
             address =  doc.data().Address;
             name = doc.data().name;
@@ -29,7 +27,6 @@ function listRestaurant() {
             if(image==""){
                 image = "Restaurant.png";
             }
-            console.log(small);
             $("#list-restaurant").append(
                 "<a href = 'restaurantPage.html?" + id + "'class = 'list-group-item list-group-item-action flex-column align-items-start restaurant-profile'>"
                     + "<img src = 'images/" + image + "' alt = 'restaurant picture'/>"    
@@ -45,5 +42,49 @@ function listRestaurant() {
         });
     });
 }
+
+// function search(){
+//     var info = document.getElementById("searchinfo").value;
+//     console.log(info);
+//     while ($("#list-restaurant").firstChild) {
+//         $("#list-restaurant").removeChild($("#list-restaurant").firstChild);
+//     }
+//     if(info!=""){
+        
+//         var id, address, image, name, cuisine, small;
+//         db.collection("restaurant").get().then(function(q){
+//             q.forEach(function(doc){
+//             id = doc.data().id;
+//             address =  doc.data().Address;
+//             name = doc.data().name;
+//             cuisine = doc.data().typeOfCuisine;
+//             image = doc.data().image;
+//             promotion = doc.data().promotion.id;
+//             if(image==""){
+//                 image = "Restaurant.png";
+//             }
+//             if(name.includes(info)||info.includes(name)){
+//                 info = name;
+//             } else if(cuisine.includes(infor)||infor.includes(cuisine)){
+//                 info = name;
+//             }
+//             {
+//                 $("#list-restaurant").append(
+//                     "<a href = 'restaurantPage.html?" + id + "'class = 'list-group-item list-group-item-action flex-column align-items-start restaurant-profile'>"
+//                         + "<img src = 'images/" + image + "' alt = 'restaurant picture'/>"    
+//                         + "<div class = 'd-flex justify-content-between' id = '" + id + "'>"
+//                         + "<h5 class = 'mb-1:'>" + name + "</h5>"
+//                         + "</div>"
+//                         + "<p class='mb-1'>"+ address.streetNumber + " " + address.streetName + ", " + address.city + "</p><p>" + address.state + " " + address.postcode + "</p>"
+//                         + "<small>" +cuisine+ "</small>"
+//                     + "</a>"    
+//                 )
+//                 displaypromotions(promotion, id);
+//             }
+            
+//             })
+//         })
+//     }
+// }
 
 listRestaurant();
