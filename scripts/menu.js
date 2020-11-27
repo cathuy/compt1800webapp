221fdf9be22f15e28e23382d4604c137a7ee69c4
Dinh Phi Long Nguyen <superitboss@gmail.com>
2020-11-27 14:28:46 -0800

    done with menu but i think there is one error with the firebase

--- scripts/menu.js
+++ scripts/menu.js
@@ -1,52 +1,39 @@
 $(document).ready(function menu() {
 
-    /*var queryString = decodeURIComponent(window.location.search);
-    var queries = queryString.split("?");   //delimiter
-    var id = queries[1];                 //get what's after '?'
-    console.log(id); */
-
-    //fid = q.data().restaurantID.id;
-
-    db.collection("dishes").get().then(function(q){
-
-        q.forEach(function(doc){
-            restaurantID = doc.data().restaurant.id;
-            console.log(restaurantID);
-        })
-        
-
-    })
+    var queryString = decodeURIComponent(window.location.search);
+    var queries = queryString.split("?"); //delimiter
+    var id = queries[1]; //get what's after '?'
+    console.log(id);
 
     db.collection("dishes").get().then(function (q) {
-        var availability ;
-        q.forEach(function(doc) {
+        q.forEach(function (doc) {
 
-            availability = doc.data().available;
-            ingredients = doc.data().ingredients;
-            image = doc.data().image;
-            name = doc.data().name;
-            price = doc.data().price;
-            id = doc.data().restaurantID;
-            //console.log(doc.data().restaurantID);
+            restaurantID = doc.data().restaurantID.id;
+            foodID = doc.data().id;
 
-            $("#menu").append(
-                "<div class='list-group-item flex-column align-items-start'>"
-                + "<div class='d-flex w-100'>"
-                    + "<img src = 'images/" + image + "' alt = 'restaurant picture'/>" 
-                    + "<div>"
-                        + "<h5 class='mb-1'>" + name + "</h5>"
-                        + "<p class='mb-1'>" + ingredients + "</p>"
-                        + "<h6 class='mb-1'>$" + price + "</h6>"
-                    + "</div>"
-                    + "<div>"
-                        + "<button class='mb-1 btn'><i class='fa fa-plus-circle'></i></button>"
-                    + "</div>"
-                + "</div>"
-                + "</div>"
-            )
-
+            if (restaurantID == id) {
+                db.collection("dishes").doc(foodID).onSnapshot(function (q) {
+                    ingredients = doc.data().ingredients;
+                    image = doc.data().image;
+                    name = doc.data().name;
+                    price = doc.data().price;
+                    $("#menu").append(
+                        "<div class='list-group-item flex-column align-items-start'>" +
+                        "<div class='d-flex w-100'>" +
+                        "<img src = 'images/" + image + "' alt = 'restaurant picture'/>" +
+                        "<div>" +
+                        "<h5 class='mb-1'>" + name + "</h5>" +
+                        "<p class='mb-1'>" + ingredients + "</p>" +
+                        "<h6 class='mb-1'>$" + price + "</h6>" +
+                        "</div>" +
+                        "<div>" +
+                        "<button class='mb-1 btn'><i class='fa fa-plus-circle'></i></button>" +
+                        "</div>" +
+                        "</div>" +
+                        "</div>"
+                    )
+                })
+            }
         })
-
-
     })
 })
